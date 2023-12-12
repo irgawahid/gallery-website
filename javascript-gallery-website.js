014d3ae17eb77e9ec8e1f460d4e0c9cbf228c733
irga123 <akun665353@gmail.com>
2023-12-13 00:27:36 +0700

    update

--- javascript-gallery-website.js
+++ javascript-gallery-website.js
@@ -10,6 +10,67 @@ hamburgerMenu.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     mobileNav.classList.remove('active');
   });
+
+  // function changeLanguage(selectElement) {
+  //   console.log("Change language function called");
+  //   var selectedLanguage = selectElement.value;
+  //   console.log("Selected language: " + selectedLanguage);}
+
+  //   function changeLanguage(selectElement) {
+  //     // Ambil nilai (value) dari opsi yang dipilih
+  //     var selectedLanguage = selectElement.value;
+
+  //     // Gantilah URL dengan URL yang sesuai berdasarkan nilai bahasa yang dipilih
+  //     switch (selectedLanguage) {
+  //         case 'id':
+  //             window.location.href = 'index.html';
+  //             break;
+  //         case 'en':
+  //             window.location.href = 'index-2.html';
+  //             break;
+  //         case 'zh':
+  //             window.location.href = 'index-3.html';
+  //             break;
+  //         case 'ja':
+  //             window.location.href = 'index-4.html';
+  //             break;
+  //         default:
+  //             // URL default jika tidak ada opsi yang cocok
+  //             window.location.href = '';
+  //             break;
+  //     }
+  // }
+
+  function changeLanguage() {
+    // Mendapatkan elemen select
+    var select = document.getElementById("language");
+
+    // Mendapatkan nilai yang dipilih
+    var selectedValue = select.options[select.selectedIndex].value;
+
+    // Menentukan URL berdasarkan nilai yang dipilih
+    var url = "";
+
+    switch (selectedValue) {
+        case "id":
+            url = "index.html"; // Ganti dengan URL bahasa Indonesia
+            break;
+        case "en":
+            url = "index-2.html"; // Ganti dengan URL bahasa Inggris
+            break;
+        case "zh":
+            url = "index-3.html"; // Ganti dengan URL bahasa China
+            break;
+        case "ja":
+            url = "index-4.html"; // Ganti dengan URL bahasa Jepang
+            break;
+        default:
+            url = "index.html"; // URL default jika tidak ada pilihan yang cocok
+    }
+
+    // Arahkan pengguna ke URL yang sesuai
+    window.location.href = url;
+}
   
 
 
